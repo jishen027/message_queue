90cc6ac8e7ef8b5a58bb7bafe1ea50ef096c6308
Jeb Lee <jishen027@gmail.com>
2024-07-25 11:56:03 +0100

    chore: Remove unnecessary files and update project structure

--- SimpleExample/Receive/Receive.cs
+++ SimpleExample/Receive/Receive.cs
@@ -1,11 +1,11 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 // Create a connection to the RabbitMQ server
 var factory = new ConnectionFactory() { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
-
 
 // Declare a queue, parameters are: queue name, durable, exclusive, autoDelete, arguments
 channel.QueueDeclare(queue: "hello",
@@ -14,9 +14,22 @@ channel.QueueDeclare(queue: "hello",
                      autoDelete: false,
                      arguments: null);
 
-const string message = "Hello World!";
 
-// Convert the message to a byte array
-var body = Encoding.UTF8.GetBytes(message);
+Console.WriteLine(" [*] Waiting for messages.");
 
-// Publish the message to the queue, parameters are: exchange, routingKey, basicProperties, body
+// Create a consumer
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+  var body = ea.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+  Console.WriteLine(" [x] Received {0}", message);
+};
+
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ SimpleExample/Send/Send.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// Create a connection to the RabbitMQ server
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+// Declare a queue, parameters are: queue name, durable, exclusive, autoDelete, arguments
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+
+// Convert the message to a byte array
+var body = Encoding.UTF8.GetBytes(message);
+
+// Publish the message to the queue, parameters are: exchange, routingKey, basicProperties, body
+channel.BasicPublish(exchange: "",
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine(" [x] Sent {0}", message);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
